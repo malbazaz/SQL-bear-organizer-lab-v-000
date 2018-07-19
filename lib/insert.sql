1a605e1956d991a0ae7aede48450366c80c7f112
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-19 14:56:51 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "blue", "naive", 1),
+(8,null, 20, "M", "light blue", "aggressive", 0);
--- lib/seed.sql
+++ lib/seed.sql
@@ -4,6 +4,6 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
 (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
 (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(6,"Grinch", 2, "M", "light Black", "Grinchy", 1),
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
 (8,null, 20, "M", "black", "aggressive", 0);
