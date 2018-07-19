3d3136a1516fe673212590e27c28b4747d63bb41
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-19 14:45:55 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,6 +5,6 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
 (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
 (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+(6,"Grinch", 2, "M", "black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "blue", "naive", 1),
+(8,null, 20, "M", "light blue", "aggressive", 0);
